bdd66e0654b3e98d63d6f777e28be74f9bd4b301
Samuel Loft <sam@devloft.co.uk>
2019-01-25 09:20:20 +0000

    Added API to get version number for application

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,14 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/*
+ * API routes that require no authentication.
+ */
+
+Route::get('version', function() {
+    return [
+        'version' => Version::major() . '.' . Version::minor() . '.' . Version::patch(),
+        'build' => Version::build()
+    ];
+});
