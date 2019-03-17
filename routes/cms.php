dbbbc622360076bc68762828ae38aa224d8a2630
Samuel Loft <sam@devloft.co.uk>
2019-03-17 19:19:17 +0000

    Added in missed middleware for CMS

--- routes/cms.php
+++ routes/cms.php
@@ -14,25 +14,29 @@ use Illuminate\Http\Request;
 
 Route::get('login', 'Auth\AdminController@showLoginForm')->name('cms.login');
 Route::post('login', 'Auth\AdminController@login')->name('cms.login.submit');
-Route::post('logout', 'Auth\AdminController@logout')->name('cms.logout');
 
-Route::get('/', 'Cms\HomeController@index')->name('cms.index');
+Route::group(['middleware' => 'auth:admin'], function () {
 
-Route::group(['prefix' => 'home-links'], function () {
-    Route::get('/', 'Cms\HomeLinksController@index')->name('cms.home-links');
-    Route::post('store', 'Cms\HomeLinksController@store')->name('cms.home-links.store');
-    Route::post('update', 'Cms\HomeLinksController@updatePositions')->name('cms.home-links.update');
-    Route::get('delete/{id}', 'Cms\HomeLinksController@destroy')->name('cms.home-links.delete');
+    Route::post('logout', 'Auth\AdminController@logout')->name('cms.logout');
 
-    Route::post('show', function(Request $request) {
-        return \App\Models\HomeLinks::show($request->id);
-    })->name('cms.home-links.show');
+    Route::get('/', 'Cms\HomeController@index')->name('cms.index');
+
+    Route::group(['prefix' => 'home-links'], function () {
+        Route::get('/', 'Cms\HomeLinksController@index')->name('cms.home-links');
+        Route::post('store', 'Cms\HomeLinksController@store')->name('cms.home-links.store');
+        Route::post('update', 'Cms\HomeLinksController@updatePositions')->name('cms.home-links.update');
+        Route::get('delete/{id}', 'Cms\HomeLinksController@destroy')->name('cms.home-links.delete');
+
+        Route::post('show', function (Request $request) {
+            return \App\Models\HomeLinks::show($request->id);
+        })->name('cms.home-links.show');
+    });
+
+    /* Product images */
+    Route::group(['prefix' => 'product-images'], function () {
+        Route::get('/', 'Cms\ProductImageController@index')->name('cms.product-images');
+        Route::post('check', 'Cms\ProductImageController@missingImages')->name('cms.product-images.check');
+        Route::post('store', 'Cms\ProductImageController@store')->name('cms.product-images.store');
+    });
+
 });
-
-/* Product images */
-Route::group(['prefix' => 'product-images'], function () {
-    Route::get('/', 'Cms\ProductImageController@index')->name('cms.product-images');
-    Route::post('check', 'Cms\ProductImageController@missingImages')->name('cms.product-images.check');
-    Route::post('store', 'Cms\ProductImageController@store')->name('cms.product-images.store');
-});
-//});
