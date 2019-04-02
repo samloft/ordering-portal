03267a05010655cc52e54106b134d885cb819197
Samuel Loft <sam@devloft.co.uk>
2019-04-02 14:04:51 +0100

    Added store method for updating company details in the ENV file (Saves a SQL query)

---
+++ app/Http/Controllers/Cms/CompanyDetailsController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Cms;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CompanyDetailsController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    /**
+     * Update the customer information in the .env file.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(Request $request)
+    {
+        $env_path = base_path('.env');
+
+        $keys = [
+            // Address Details
+            'ADDRESS_LINE_1' => $request->address_line_1,
+            'ADDRESS_LINE_2' => $request->address_line_2,
+            'ADDRESS_LINE_3' => $request->address_line_3,
+            'ADDRESS_LINE_4' => $request->address_line_4,
+            'ADDRESS_LINE_5' => $request->address_line_5,
+            'ADDRESS_POSTCODE' => $request->postcode,
+            'ADDRESS_TELEPHONE' => $request->telephone,
+            'ADDRESS_FAX' => $request->fax,
+            'ADDRESS_EMAIL' => $request->email,
+
+            // Social Details
+            'SOCIAL_FACEBOOK' => $request->facebook,
+            'SOCIAL_LINKEDIN' => $request->linkedin,
+            'SOCIAL_TWITTER' => $request->twitter,
+            'SOCIAL_INSTAGRAM' => $request->instagram,
+
+            // App Details
+            'APP_APPLE' => $request->apple,
+            'APP_ANDROID' => $request->android,
+        ];
+
+        foreach ($keys as $key => $value) {
+            file_put_contents($env_path, str_replace(
+                $key . '="' . env($key) . '"', $key . '="' . $value . '"', file_get_contents($env_path)
+            ));
+        }
+
+        return back()->with('success', 'Company details have been updated');
+    }
+}
