8b25e67fc7cf3635a29e0e7401d7cfc23a249599
Samuel Loft <sam@devloft.co.uk>
2019-04-12 11:18:47 +0100

    Added account summary migration

---
+++ database/migrations/2019_04_12_094440_create_account_summary_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccountSummaryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('account_summary', function (Blueprint $table) {
+            $table->string('customer_code')->limit(8);
+            $table->string('item_no')->limit(20);
+            $table->string('reference')->limit(20);
+            $table->date('dated');
+            $table->date('due_date');
+            $table->float('unall_curr_amount');
+            $table->string('age');
+
+            $table->primary(['customer_code', 'item_no']);
+            $table->index(['customer_code', 'item_no']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('account_summary');
+    }
+}
