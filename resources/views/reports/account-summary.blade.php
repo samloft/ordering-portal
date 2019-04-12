b5cd344d8edfa76e4f7d19cc2c4c24f6d9b1e100
Samuel Loft <sam@devloft.co.uk>
2019-04-12 16:20:22 +0100

    Change account summary PDF to have in-line styles (Potential speed increase)

--- resources/views/reports/account-summary.blade.php
+++ resources/views/reports/account-summary.blade.php
@@ -3,14 +3,14 @@
 @section('report.title', 'Account Summary')
 
 @section('content')
-    <p class="table-heading">{{ __('Outstanding Invoices & Credit Notes') }}</p>
+    <p style="text-align: center; font-weight: 900;">{{ __('Outstanding Invoices & Credit Notes') }}</p>
 
-    <div class="small-print">
+    <div style="font-size: 10px; margin-bottom: 5px;">
         {{ __('The table below lists all unpaid invoices and credit notes, there is a delay of up to 24 hours in the accuracy of our data, please accept our apologies if you have paid an invoice that is listed below. If you have any queries about the data shown below please contact us.') }}
     </div>
 
-    <table>
-        <thead>
+    <table border="1" style="width: 100%; border-collapse: collapse;">
+{{--        <thead>--}}
         <tr>
             <th>{{ __('Invoice No.') }}</th>
             <th>{{ __('Order No.') }}</th>
@@ -18,17 +18,17 @@
             <th>{{ __('Due Date') }}</th>
             <th>{{ __('Amount') }}</th>
         </tr>
-        </thead>
-        <tbody>
+{{--        </thead>--}}
+{{--        <tbody>--}}
         @foreach($invoice_lines as $invoice_line)
             <tr>
                 <td>{{ $invoice_line->item_no }}</td>
                 <td>{{ $invoice_line->reference }}</td>
                 <td>{{ \Carbon\Carbon::parse($invoice_line->dated)->format('d-m-Y') }}</td>
                 <td>{{ \Carbon\Carbon::parse($invoice_line->due_date)->format('d-m-Y') }}</td>
-                <td class="text-right">{{ $invoice_line->unall_curr_amount }}</td>
+                <td style="text-align: right;">{{ $invoice_line->unall_curr_amount }}</td>
             </tr>
         @endforeach
-        </tbody>
+{{--        </tbody>--}}
     </table>
 @endsection
--- resources/views/reports/master-pdf.blade.php
+++ resources/views/reports/master-pdf.blade.php
@@ -2,102 +2,102 @@
 <head>
     <title>@yield('reports.title')</title>
 
-    <style>
-        body {
-            font-family: Arial, serif;
-            font-size: 12px;
-        }
+{{--    <style>--}}
+{{--        body {--}}
+{{--            font-family: Arial, serif;--}}
+{{--            font-size: 12px;--}}
+{{--        }--}}
 
-        table {
-            width: 100%;
-            border-collapse: collapse;
-        }
+{{--        table {--}}
+{{--            width: 100%;--}}
+{{--            border-collapse: collapse;--}}
+{{--        }--}}
 
-        table th {
-            padding-top: 10px;
-            padding-bottom: 10px;
-        }
+{{--        table th {--}}
+{{--            padding-top: 10px;--}}
+{{--            padding-bottom: 10px;--}}
+{{--        }--}}
 
-        table td {
-            padding-top: 5px;
-            padding-bottom: 5px;
-        }
+{{--        table td {--}}
+{{--            padding-top: 5px;--}}
+{{--            padding-bottom: 5px;--}}
+{{--        }--}}
 
-        table, th, td {
-            border: 0.5px solid rgb(0, 0, 0);
-        }
-
+{{--        table, th, td {--}}
+{{--            border: 0.5px solid rgb(0, 0, 0);--}}
+{{--        }--}}
+<style>
         th, td {
             padding-left: 5px;
             padding-right: 5px;
         }
+        </style>
 
-        table th.description {
-            width: 270px;
-        }
+{{--        table th.description {--}}
+{{--            width: 270px;--}}
+{{--        }--}}
 
-        table th.outstanding {
-            width: 50px;
-        }
+{{--        table th.outstanding {--}}
+{{--            width: 50px;--}}
+{{--        }--}}
 
-        .table-heading {
-            text-align: center;
-            font-weight: 900;
-        }
+{{--        .table-heading {--}}
+{{--            text-align: center;--}}
+{{--            font-weight: 900;--}}
+{{--        }--}}
 
-        .small-print {
-            font-size: 10px;
-            margin-bottom: 5px;
-        }
+{{--        .small-print {--}}
+{{--            font-size: 10px;--}}
+{{--            margin-bottom: 5px;--}}
+{{--        }--}}
 
-        .text-right {
-            text-align: right !important;
-        }
+{{--        .text-right {--}}
+{{--            text-align: right !important;--}}
+{{--        }--}}
 
-        .row {
-            width: 100%;
-        }
+{{--        .row {--}}
+{{--            width: 100%;--}}
+{{--        }--}}
 
-        .col {
-            display: inline-block;
-            width: 50%;
-        }
+{{--        .col {--}}
+{{--            display: inline-block;--}}
+{{--            width: 50%;--}}
+{{--        }--}}
 
-        .contact-numbers {
-            margin-top: 10px;
-        }
+{{--        .contact-numbers {--}}
+{{--            margin-top: 10px;--}}
+{{--        }--}}
 
-        /*.address span {*/
-        /*    display: block;*/
-        /*}*/
+{{--        /*.address span {*/--}}
+{{--        /*    display: block;*/--}}
+{{--        /*}*/--}}
 
-        h3.title {
-            font-weight: 900;
-            font-size: 18px;
-            text-align: center;
-        }
+{{--        h3.title {--}}
+{{--            font-weight: 900;--}}
+{{--            font-size: 18px;--}}
+{{--            text-align: center;--}}
+{{--        }--}}
 
-        /*.details span.heading {*/
-        /*    float: left;*/
-        /*    font-weight: 900;*/
-        /*    width: 100px !important;*/
-        /*}*/
+{{--        /*.details span.heading {*/--}}
+{{--        /*    float: left;*/--}}
+{{--        /*    font-weight: 900;*/--}}
+{{--        /*    width: 100px !important;*/--}}
+{{--        /*}*/--}}
 
-        img.logo {
-            background-color: #b9c5d3;
-            padding: 20px;
-            max-height: 55px;
-        }
-    </style>
+{{--        img.logo {--}}
+{{--            background-color: #b9c5d3;--}}
+{{--            padding: 20px;--}}
+{{--            max-height: 55px;--}}
+{{--        }--}}
+{{--    </style>--}}
 </head>
-<body>
+<body style="font-family: Arial, serif; font-size: 12px;">
 
-<div class="header row">
-    <div class="col">
-{{--        <img class="logo" src="{{ asset('images/logo.png') }}" alt="Image not found">--}}
-        <img class="logo" src="https://scolmoreonline.com/assets/images/scolmore_small.png" alt="Image not found">
+<div style="width: 100%;">
+    <div style="display: inline-block; width: 50%;">
+        <img src="https://scolmoreonline.com/assets/images/scolmore_small.png" alt="Image not found" style="background-color: #b9c5d3; padding: 20px; max-height: 55px;">
     </div>
-    <div class="col text-right address">
+    <div style="text-align: right; display: inline-block; width: 50%;">
         <div>{{ env('ADDRESS_LINE_1') }}</div>
         <div>{{ env('ADDRESS_LINE_2') }}</div>
         <div>{{ env('ADDRESS_LINE_3') }}</div>
@@ -105,21 +105,21 @@
         <div>{{ env('ADDRESS_LINE_5') }}</div>
         <div>{{ env('ADDRESS_POSTCODE') }}</div>
 
-        <div class="contact-numbers">
+        <div style="margin-top: 10px;">
             <div>{{ env('ADDRESS_TELEPHONE') ? 'Tel: ' .  env('ADDRESS_TELEPHONE') : '' }}</div>
             <div>{{ env('ADDRESS_FAX') ? 'Fax: ' .  env('ADDRESS_FAX') : '' }}</div>
         </div>
     </div>
 </div>
 
-<h3 class="title">@yield('report.title')</h3>
+<h3 style="font-weight: 900; font-size: 18px; text-align: center;">@yield('report.title')</h3>
 
-<div class="details">
+<div>
     <p>
-        <strong>{{ __('Account Code: ') }}</strong>{{ Auth::user()->customer_code }}
+        <b>{{ __('Account Code: ') }}</b>{{ Auth::user()->customer_code }}
     </p>
     <p>
-        <strong>{{ __('Customer: ') }}</strong>{{ Auth::user()->customer->customer_name }}
+        <b>{{ __('Customer: ') }}</b>{{ Auth::user()->customer->customer_name }}
     </p>
 </div>
 
