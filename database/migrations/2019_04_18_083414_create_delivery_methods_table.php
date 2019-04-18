9a3ac67f995df5a3b5fa8256bd1fe3cc54825abe
Samuel Loft <sam@devloft.co.uk>
2019-04-18 09:50:53 +0100

    Added delivery method migration

---
+++ database/migrations/2019_04_18_083414_create_delivery_methods_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDeliveryMethodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('delivery_methods', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->uuid('uuid');
+            $table->string('title');
+            $table->string('identifier');
+            $table->string('code');
+            $table->integer('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('delivery_methods');
+    }
+}
