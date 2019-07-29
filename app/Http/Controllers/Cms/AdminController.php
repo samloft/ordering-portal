c5ed68f731ee56092713c1513666c596bde46601
Samuel Loft <sam@devloft.co.uk>
2019-07-29 14:59:35 +0100

    Added admin page controller

---
+++ app/Http/Controllers/Cms/AdminController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Cms;
+
+use App\Models\Admin;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\View\View;
+
+class AdminController extends Controller
+{
+    /**
+     * @return Factory|View
+     */
+    public function index()
+    {
+        $users = Admin::show(10);
+
+        return view('cms.admin.index', compact('users'));
+    }
+}
