ebec53b523e268d562671b52dd6cce3cd7b2c3cc
Samuel Loft <sam@scolmore.com>
2019-10-05 16:15:47 +0100

    Removed viewbox from alert SVGs

--- resources/views/layout/alerts.blade.php
+++ resources/views/layout/alerts.blade.php
@@ -1,7 +1,7 @@
 @if ($errors->any() || session('error'))
     <div class="alert alert-danger" role="alert">
         <div class="alert-body">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="alert-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" class="alert-icon">
                 <circle cx="12" cy="12" r="10" class="primary"></circle>
                 <path class="secondary"
                       d="M13.41 12l2.83 2.83a1 1 0 0 1-1.41 1.41L12 13.41l-2.83 2.83a1 1 0 1 1-1.41-1.41L10.59 12 7.76 9.17a1 1 0 0 1 1.41-1.41L12 10.59l2.83-2.83a1 1 0 0 1 1.41 1.41L13.41 12z"></path>
@@ -17,7 +17,7 @@
 @if (session('status') || session('success'))
     <div class="alert alert-success" role="alert">
         <div class="alert-body">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="alert-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" class="alert-icon">
                 <circle cx="12" cy="12" r="10" class="primary"></circle>
                 <path class="secondary"
                       d="M10 14.59l6.3-6.3a1 1 0 0 1 1.4 1.42l-7 7a1 1 0 0 1-1.4 0l-3-3a1 1 0 0 1 1.4-1.42l2.3 2.3z"></path>
