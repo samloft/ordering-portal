e7033032a90bd7e80d70e52f9b23bbcd9a7cfd64
Samuel Loft <sam@scolmore.com>
2019-10-14 12:21:27 +0100

    Added testing database

--- config/database.php
+++ config/database.php
@@ -56,10 +56,20 @@ return [
             'engine' => null,
         ],
 
-        'sqlite_testing' => [
-            'driver'   => 'sqlite',
-            'database' => storage_path().'/testing.sqlite',
-            'prefix'   => '',
+        'testing' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
         ],
 
         'pgsql' => [
