b4e22b32cbfecbbaddf65678498a1b8d45b9dcb2
Samuel Loft <sam@scolmore.com>
2019-10-14 11:42:58 +0100

    Fix for 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,18 +1,12 @@
-@extends('layout.master')
-
-@section('page.title', '404 Page Not Found')
-
-@section('content')
-    <div class="card card-body">
-        <div class="text-center">
-            <img src="https://media1.tenor.com/images/f5b11c5564ee186d00a2b4ff6e027bd7/tenor.gif?itemid=7325938">
-            <h1 class="mt-3">{{ __('404! Oh No! Oh Dear! Page not found.') }}</h1>
-            <p>
-                {{ __('It would seem you have navigated to a page that not longer or has ever existed.') }}
-            </p>
-            <a href="{{ route('home') }}">
-                <button class="btn btn-primary">Return Home</button>
-            </a>
-        </div>
+<div class="card card-body">
+    <div class="text-center">
+        <img src="https://media1.tenor.com/images/f5b11c5564ee186d00a2b4ff6e027bd7/tenor.gif?itemid=7325938">
+        <h1 class="mt-3">{{ __('404! Oh No! Oh Dear! Page not found.') }}</h1>
+        <p>
+            {{ __('It would seem you have navigated to a page that not longer or has ever existed.') }}
+        </p>
+        <a href="{{ route('home') }}">
+            <button class="btn btn-primary">Return Home</button>
+        </a>
     </div>
-@endsection
+</div>
