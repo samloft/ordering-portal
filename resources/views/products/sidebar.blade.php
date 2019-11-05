34d66d5bd041ebc41e7ba315b9dedaf19dd9da1c
Samuel Loft <sam@scolmore.com>
2019-11-05 13:06:45 +0000

    Improved sidebar styling

--- resources/views/products/sidebar.blade.php
+++ resources/views/products/sidebar.blade.php
@@ -1,6 +1,6 @@
-<div class="w-1/4 pr-5">
+<div class="w-1/4 pr-5 sidebar">
     @if (auth()->user()->admin || count(auth()->user()->customers) > 0)
-        <h4 class="uppercase mb-3 font-medium tracking-wider">{{ __('Change Customer') }}</h4>
+        <h4>{{ __('Change Customer') }}</h4>
 
         <form method="POST" action="{{ route('customer.change') }}">
             @if (auth()->user()->admin)
@@ -33,28 +33,18 @@
         </form>
     @endif
 
-    <div class="row">
-        <div class="col">
+    <h4>{{ __('Search') }}</h4>
 
-            <h4 class="uppercase mb-3 font-medium tracking-wider">{{ __('Search') }}</h4>
-
-            <form class="w-100" action="{{ route('products.search') }}" method="get">
-                <div class="form-group">
-                    <div class="input-group">
-                        <div class="relative">
-                            <input type="text" class="form-control" name="query" autocomplete="off">
-                            <div
-                                class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-3 text-gray-700">
-                                <button class="input-group-text"><i class="fas fa-search"></i></button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </form>
+    <form class="w-100" action="{{ route('products.search') }}" method="get">
+        <div class="relative">
+            <input type="text" class="form-control" name="query" autocomplete="off" required>
+            <button class="absolute inset-y-0 right-0 flex items-center px-3 text-gray-700">
+                <i class="fas fa-search"></i>
+            </button>
         </div>
-    </div>
+    </form>
 
-    <h4 class="uppercase mb-3 font-medium tracking-wider">{{ __('Quick Buy') }}</h4>
+    <h4>{{ __('Quick Buy') }}</h4>
 
     <form method="post" id="product-add-basket-quickbuy">
         <input id="quick-buy" class="mb-2" name="product"
@@ -70,52 +60,48 @@
         </div>
     </form>
 
-    <div class="row mb-2">
-        <div class="col">
-            <h4 class="uppercase mb-3 font-medium tracking-wider">{{ __('Categories') }}</h4>
+    <h4>{{ __('Categories') }}</h4>
 
-            <ul class="">
-                @foreach($category_list as $key => $category)
-                    <li class="py-2">
-                        <a href="{{ route('products', $category_list[$key]['url']) }}">
-                            {{ $category_list[$key]['name'] }}
-                            @if (count($category_list[$key]['sub']) > 0)
-                                <span class="float-right pt-1">
+    <ul class="categories-nav">
+        @foreach($category_list as $key => $category)
+            <li class="top-level">
+                <a href="{{ route('products', $category_list[$key]['url']) }}">
+                    {{ $category_list[$key]['name'] }}
+                    @if (count($category_list[$key]['sub']) > 0)
+                        <span class="float-right pt-1">
                                 <i class="fas {{ $categories['level_1'] === $category_list[$key]['name'] ? 'fa-chevron-down' : 'fa-chevron-right' }}"></i>
                             </span>
-                            @endif
-                        </a>
-                    </li>
-                    @if ($categories['level_1'] === $category_list[$key]['name'] && count($category_list[$key]['sub']) > 0)
-                        <ul class="">
-                            @foreach ($category_list[$key]['sub'] as $level_2)
-                                <li class="py-1 pl-2">
-                                    <a href="{{ route('products', [$category_list[$key]['url'], $level_2['url']]) }}"
-                                       class="{{ $categories['level_2'] === $level_2['name'] ? 'font-weight-bold' : '' }}">
-                                        {{ $level_2['name'] }}
-                                        @if (count($level_2['sub']) > 0)
-                                            <span class="float-right pt-1"><i
-                                                    class="fas {{ $categories['level_2'] === $level_2['name'] ? 'fa-chevron-down' : 'fa-chevron-right' }}"></i></span>
-                                        @endif
-                                    </a>
-                                </li>
-                                @if ($categories['level_2'] === $level_2['name'] && count($level_2['sub']) > 0)
-                                    <ul class="pl-4">
-                                        @foreach ($level_2['sub'] as $level_3)
-                                            <li class="py-1">
-                                                <a href="{{ route('products', [$category_list[$key]['url'], $level_2['url'], $level_3['url']]) }}"
-                                                   class="{{ $categories['level_3'] === $level_3['name'] ? 'font-weight-bold' : '' }}">
-                                                    {{ $level_3['name'] }}
-                                                </a>
-                                            </li>
-                                        @endforeach
-                                    </ul>
-                                @endif
-                            @endforeach
-                        </ul>
                     @endif
-                @endforeach
-            </ul>
-        </div>
-    </div>
+                </a>
+            </li>
+            @if ($categories['level_1'] === $category_list[$key]['name'] && count($category_list[$key]['sub']) > 0)
+                <ul class="sub-categories">
+                    @foreach ($category_list[$key]['sub'] as $level_2)
+                        <li class="py-1 pl-2">
+                            <a href="{{ route('products', [$category_list[$key]['url'], $level_2['url']]) }}"
+                               class="{{ $categories['level_2'] === $level_2['name'] ? 'active' : '' }}">
+                                {{ $level_2['name'] }}
+                                @if (count($level_2['sub']) > 0)
+                                    <span class="float-right pt-1"><i
+                                            class="fas {{ $categories['level_2'] === $level_2['name'] ? 'fa-chevron-down' : 'fa-chevron-right' }}"></i></span>
+                                @endif
+                            </a>
+                        </li>
+                        @if ($categories['level_2'] === $level_2['name'] && count($level_2['sub']) > 0)
+                            <ul class="pl-4">
+                                @foreach ($level_2['sub'] as $level_3)
+                                    <li class="py-1">
+                                        <a href="{{ route('products', [$category_list[$key]['url'], $level_2['url'], $level_3['url']]) }}"
+                                           class="{{ $categories['level_3'] === $level_3['name'] ? 'active' : '' }}">
+                                            {{ $level_3['name'] }}
+                                        </a>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        @endif
+                    @endforeach
+                </ul>
+            @endif
+        @endforeach
+    </ul>
 </div>
