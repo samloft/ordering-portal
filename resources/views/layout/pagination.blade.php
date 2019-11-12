8786246cd2536498e90780f4eb1005be991f26d7
Samuel Loft <sam@scolmore.com>
2019-11-12 10:18:40 +0000

    Removed redundant class names

--- resources/views/layout/pagination.blade.php
+++ resources/views/layout/pagination.blade.php
@@ -1,39 +1,36 @@
 @if ($paginator->hasPages())
     <div class="pagination">
         <ul>
-            {{-- Previous Page Link --}}
             @if ($paginator->onFirstPage())
-                <li class="page-item disabled">
-                    <a class="page-link" href="#" aria-label="Previous">
+                <li class="disabled">
+                    <a href="" aria-label="Previous">
                         <span aria-hidden="true">&laquo;</span>
                         <span class="sr-only">Previous</span>
                     </a>
                 </li>
             @else
-                <li class="page-item">
-                    <a href="{{ $paginator->previousPageUrl() }}" class="page-link" rel="prev">&laquo;</a>
+                <li>
+                    <a href="{{ $paginator->previousPageUrl() }}" rel="prev">&laquo;</a>
                 </li>
             @endif
 
-            {{-- Pagination Elements --}}
             @foreach ($elements as $element)
-                {{-- "Three Dots" Separator --}}
+
                 @if (is_string($element))
-                    <li class="page-item disabled">{{ $element }}</li>
+                    <li class="disabled">{{ $element }}</li>
                 @endif
 
-                {{-- Array Of Links --}}
                 @if (is_array($element))
                     @foreach ($element as $page => $url)
                         @if ($page === $paginator->currentPage())
-                            <li class="page-item active">
-                                <a href="#" class="page-link">
+                            <li class="active">
+                                <a href="#">
                                     {{ $page }}<span class="sr-only">(current)</span>
                                 </a>
                             </li>
                         @else
-                            <li class="page-item">
-                                <a href="{{ $url }}" class="page-link">
+                            <li>
+                                <a href="{{ $url }}">
                                     {{ $page }}
                                 </a>
                             </li>
@@ -42,14 +39,13 @@
                 @endif
             @endforeach
 
-            {{-- Next Page Link --}}
             @if ($paginator->hasMorePages())
-                <li class="page-item">
-                    <a href="{{ $paginator->nextPageUrl() }}" class="page-link" rel="next">&raquo;</a>
+                <li>
+                    <a href="{{ $paginator->nextPageUrl() }}" rel="next">&raquo;</a>
                 </li>
             @else
-                <li class="page-item disabled">
-                    <a class="page-link" href="#" aria-label="Next">
+                <li class="disabled">
+                    <a href="#" aria-label="Next">
                         <span aria-hidden="true">&raquo;</span>
                         <span class="sr-only">Next</span>
                     </a>
