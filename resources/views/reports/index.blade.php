563dd16cd5c8c634028b380cabea6ed1cbc84529
Samuel Loft <sam@devloft.co.uk>
2019-12-02 15:58:10 +0000

    Added styling for the report page

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -3,37 +3,75 @@
 @section('page.title', 'Reports')
 
 @section('content')
-    <h1 class="page-title">{{ __('Reports') }}</h1>
+    <div class="w-full mb-5 text-center">
+        <h2 class="font-semibold tracking-widest">{{ __('Reports') }}</h2>
+        <p class="font-thin">
+            {{ __('Select a report and output type you would like to download.') }}
+        </p>
+    </div>
 
-    <div class="card card-body">
+    <div class="bg-white rounded-lg shadow p-4 text-center">
         @include('layout.alerts')
 
-        <div class="text-center">
-            <form id="run-report" action="{{ route('reports.show') }}" method="post">
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" name="report" value="account_summary" autocomplete="off">
-                    <label class="form-check-label">
-                        {{ __('Account Summary') }}
-                    </label>
-                </div>
+        <form id="run-report" action="{{ route('reports.show') }}" method="post" class="m-0 ">
+            <label class="checkbox flex items-center justify-center mb-1">
+                <input type="checkbox" class="form-checkbox" name="report" value="account_summary">
+                <span class="ml-2">{{ __('Account Summary') }}</span>
+            </label>
 
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" name="report" value="back_orders" autocomplete="off">
-                    <label class="form-check-label">
-                        {{ __('Back Order History') }}
-                    </label>
-                </div>
+            <label class="checkbox flex items-center justify-center">
+                <input type="checkbox" class="form-checkbox" name="report">
+                <span class="ml-2">{{ __('Back Order History') }}</span>
+            </label>
 
-                <div class="form-group form-inline justify-content-center mt-3">
-                    <label class="mr-1">{{ __('Output As: ') }}</label>
-                    <select class="form-control form-control-sm" name="output" autocomplete="off">
-                        <option value="pdf">{{ __('PDF') }}</option>
-                        <option value="csv">{{ __('CSV') }}</option>
-                    </select>
-                </div>
+            <div class="relative mt-3 w-40 mx-auto">
+                <label for="output">{{ __('Output As') }} </label>
+                <select class="w-full p-2 rounded border text-gray-600 appearance-none" name="output">
+                    <option value="pdf">{{ __('PDF') }}</option>
+                    <option value="csv">{{ __('CSV') }}</option>
+                </select>
 
-                <button type="submit" class="btn btn-primary mt-4">{{ __('Run Report') }}</button>
-            </form>
-        </div>
+                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-3 pt-6 text-gray-700">
+                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
+                    </svg>
+                </div>
+            </div>
+
+            <button class="button button-primary mt-5">{{ __('Run Report') }}</button>
+        </form>
     </div>
+
+    {{--    <div class="card card-body">--}}
+    {{--        @include('layout.alerts')--}}
+
+    {{--        <div class="text-center">--}}
+    {{--            <form id="run-report" action="{{ route('reports.show') }}" method="post">--}}
+    {{--                <div class="form-check">--}}
+    {{--                    <input class="form-check-input" type="radio" name="report" value="account_summary"--}}
+    {{--                           autocomplete="off">--}}
+    {{--                    <label class="form-check-label">--}}
+    {{--                        {{ __('Account Summary') }}--}}
+    {{--                    </label>--}}
+    {{--                </div>--}}
+
+    {{--                <div class="form-check">--}}
+    {{--                    <input class="form-check-input" type="radio" name="report" value="back_orders" autocomplete="off">--}}
+    {{--                    <label class="form-check-label">--}}
+    {{--                        {{ __('Back Order History') }}--}}
+    {{--                    </label>--}}
+    {{--                </div>--}}
+
+    {{--                <div class="form-group form-inline justify-content-center mt-3">--}}
+    {{--                    <label class="mr-1">{{ __('Output As: ') }}</label>--}}
+    {{--                    <select class="form-control form-control-sm" name="output" autocomplete="off">--}}
+    {{--                        <option value="pdf">{{ __('PDF') }}</option>--}}
+    {{--                        <option value="csv">{{ __('CSV') }}</option>--}}
+    {{--                    </select>--}}
+    {{--                </div>--}}
+
+    {{--                <button type="submit" class="btn btn-primary mt-4">{{ __('Run Report') }}</button>--}}
+    {{--            </form>--}}
+    {{--        </div>--}}
+    {{--    </div>--}}
 @endsection
