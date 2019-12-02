842b08ff74a28839177ee5beed758b2131b24b58
Samuel Loft <sam@devloft.co.uk>
2019-12-02 15:05:24 +0000

    Removed markup as no longer required

--- resources/views/saved-baskets/show.blade.php
+++ resources/views/saved-baskets/show.blade.php
@@ -28,36 +28,6 @@
         </div>
     </div>
 
-    {{--    <div class="row">--}}
-    {{--        <div class="col-lg-4">--}}
-    {{--            <div class="card card-body">--}}
-
-    {{--                <div class="row mb-2">--}}
-    {{--                    <div class="col">{{ ('Template Reference:') }}</div>--}}
-    {{--                    <div class="col">{{ $saved_basket->first()->reference }}</div>--}}
-    {{--                </div>--}}
-
-    {{--                <div class="row mb-2">--}}
-    {{--                    <div class="col">{{ ('Date Saved:') }}</div>--}}
-    {{--                    <div class="col">{{ date('d/m/Y', strtotime($saved_basket->first()->created_at)) }}</div>--}}
-    {{--                </div>--}}
-
-    {{--                <div class="mt-3 row">--}}
-    {{--                    <div class="col">--}}
-    {{--                        <button class="btn btn-blue btn-block" onclick="window.history.back();">--}}
-    {{--                            {{ __('Back') }}--}}
-    {{--                        </button>--}}
-    {{--                    </div>--}}
-    {{--                    <div class="col">--}}
-    {{--                        <a class="btn-link"--}}
-    {{--                           href="{{ route('saved-baskets.destroy', ['id' => $saved_basket->first()->id]) }}">--}}
-    {{--                            <button class="btn btn-outline-danger btn-block">{{ __('Delete Template') }}</button>--}}
-    {{--                        </a>--}}
-    {{--                    </div>--}}
-    {{--                </div>--}}
-    {{--            </div>--}}
-    {{--        </div>--}}
-
     <h2 class="font-semibold tracking-widest mt-3 mb-3">{{ __('Products') }}</h2>
 
     <table>
@@ -88,5 +58,4 @@
             <button class="button button-primary">{{ __('Add to basket') }}</button>
         </a>
     </div>
-    {{--    </div>--}}
 @endsection
