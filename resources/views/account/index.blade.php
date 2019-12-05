b1e1316c00a9f1763c29263be3605bc442941f38
Samuel Loft <sam@devloft.co.uk>
2019-12-05 08:17:36 +0000

    Added styling for the users account page

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -3,150 +3,125 @@
 @section('page.title', 'Account')
 
 @section('content')
-    <h1 class="page-title">{{ __('Your Account') }}</h1>
+    <div class="w-full mb-5 text-center">
+        <h2 class="font-semibold tracking-widest">{{ __('Your Account') }}</h2>
+        <p class="font-thin">
+            {{ __('Update information regarding your online account') }}
+        </p>
+    </div>
 
-    <div class="row">
-        <div class="col-lg-5 d-flex align-items-stretch">
-            <div class="card card-body">
-                <h2 class="section-title">{{ __('Delivery Address') }}</h2>
-                <div class="address">
-                    @if ($default_address)
-                        <div class="address">
-                            <span>{{ $default_address->address_line_1 }}</span>
-                            <span>{{ $default_address->address_line_2 }}</span>
-                            <span>{{ $default_address->address_line_3 }}</span>
-                            <span>{{ $default_address->address_line_4 }}</span>
-                            <span>{{ $default_address->address_line_5 }}</span>
-                            <span>{{ $default_address->post_code }}</span>
+    <div class="bg-white rounded shadow-md p-3 mb-5 mr-2">
+        <div class="flex">
+            <div class="w-1/3 mr-5">
+                <h4 class="text-primary">{{ __('Your Details') }}</h4>
+                <p class="text-gray-500 font-thin">
+                    {{ __('Update your account details for your online ordering account') }}
+                </p>
+
+                <div role="alert" class="alert alert-info mt-5">
+                    <div class="alert-body text-sm items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="alert-icon">
+                            <path d="M12 2a10 10 0 1 1 0 20 10 10 0 0 1 0-20z" class="primary"></path>
+                            <path
+                                d="M11 12a1 1 0 0 1 0-2h2a1 1 0 0 1 .96 1.27L12.33 17H13a1 1 0 0 1 0 2h-2a1 1 0 0 1-.96-1.27L11.67 12H11zm2-4a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"
+                                class="secondary"></path>
+                        </svg>
+                        <div>
+                            <p class="alert-text">{{ __('Should you require to change your email address, you will need to contact customer services') }}</p>
                         </div>
-                    @else
-                        <span class="text-muted">{{ __('No default delivery address set, click below to add one.') }}</span>
-                    @endif
-
-                    <div class="text-right mt-2">
-                        <a href="{{ route('account.addresses') }}" class="btn-link">
-                            <button class="btn btn-blue">{{ __('Address') }}</button>
-                        </a>
                     </div>
                 </div>
 
-                <h2 class="section-title mt-3">{{ __('Invoice Address') }}</h2>
-                <div class="address">
-                    <span>{{ Auth::user()->customer->invoice_customer_name }}</span>
-                    <span>{{ Auth::user()->customer->invoice_customer_address_line_1 }}</span>
-                    <span>{{ Auth::user()->customer->invoice_customer_address_line_2 }}</span>
-                    <span>{{ Auth::user()->customer->invoice_customer_address_line_3 }}</span>
-                    <span>{{ Auth::user()->customer->invoice_customer_address_line_4 }}</span>
-                    <span>{{ Auth::user()->customer->invoice_customer_address_line_5 }}</span>
+                <div class="flex align-items">
+                    <span class="mr-3">{{ __('Current Email') }}</span>
+                    <span class="badge badge-success">{{ auth()->user()->email }}</span>
                 </div>
-
-                <h2 class="section-title mt-3">{{ __('Change Password') }}</h2>
-                {{ __('Click below to change your password.') }}
-                <div class="text-right mt-2">
-                    <a href="{{ route('account.password') }}" class="btn-link">
-                        <button class="btn btn-blue">{{ __('Change Password') }}</button>
-                    </a>
-                </div>
-
-                <h2 class="section-title mt-3">{{ __('API') }}</h2>
-                <p>{{ __('Here is your unique API token to give you access to the Scolmore API') }}</p>
-
-                <div class="input-group">
-                <input class="form-control"  value="{{ auth()->user()->api_token }}" disabled>
-                    <div class="input-group-btn">
-                        <button class="btn btn-default" type="button"><i class="fa fa-copy"></i></button>
-                    </div>
-                </div>
-
-                <a href="#" class="btn btn-blue btn-block mt-3">{{ __('View Documentation') }}</a>
-
             </div>
-        </div>
-        <div class="col-lg-7 d-flex align-items-stretch">
-            <div class="card card-body">
-                <h2 class="section-title">{{ __('Company Details') }}</h2>
-                <div class="row">
-                    <label class="col-sm-4 col-form-label">{{ __('Account Code') }}</label>
-                    <div class="col-sm-8">
-                        <input class="form-control-plaintext"
-                               placeholder="{{ Auth::user()->customer->customer_code }}">
+            <div class="w-2/3">
+                <form method="post" action="{{ route('account.store') }}" class="m-0">
+                    <div class="border border-gray-300 rounded p-3 text-center leading-tight mb-5">
+                        <span class="font-medium block">{{ auth()->user()->customer->code }}</span>
+                        <span class="lock text-gray-600 font-thin">{{ auth()->user()->customer->name }}</span>
                     </div>
-                </div>
-
-                <div class="row">
-                    <label class="col-sm-4 col-form-label">{{ __('Customer Name') }}</label>
-                    <div class="col-sm-8">
-                        <input class="form-control-plaintext"
-                               placeholder="{{ Auth::user()->customer->customer_name }}">
-                    </div>
-                </div>
-
-                <form method="post" action="{{ route('account.store') }}">
-                    <h2 class="section-title mt-3">{{ __('Contact Details') }}</h2>
-
-                    @include('layout.alerts')
 
                     <div class="mb-3">
-                        <span class="required"></span> <span class="text-muted">{{ __('Denotes a required field') }}</span>
+                        <label for="name" class="text-sm">{{ __('Name') }}</label>
+                        <input id="name" name="name" class="bg-gray-100"
+                               value="{{ old('name') ?: auth()->user()->name }}">
                     </div>
 
-                    <div class="form-group row">
-                        <label class="col-sm-4 col-form-label">{{ __('First Name') }}<span class="required"></span></label>
-                        <div class="col-sm-8">
-                            <input class="form-control" name="first_name" value="{{ Auth::user()->first_name }}">
+                    <div class="mb-3">
+                        <label for="name" class="text-sm">{{ __('Password') }}</label>
+                        <input id="name" name="name" class="bg-gray-100"
+                               value="{{ old('password') ?: '************' }}">
+                    </div>
+
+                    <div class="mb-3 flex">
+                        <div class="mr-2 w-full">
+                            <label for="telephone" class="text-sm">{{ __('Telephone') }}</label>
+                            <input id="telephone" name="telephone" class="bg-gray-100"
+                                   value="{{ old('telephone') ?: auth()->user()->telephone }}">
                         </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label class="col-sm-4 col-form-label">{{ __('Surname') }}<span class="required"></span></label>
-                        <div class="col-sm-8">
-                            <input class="form-control" name="last_name" value="{{ Auth::user()->last_name }}">
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label class="col-sm-4 col-form-label">{{ __('Telephone') }}</label>
-                        <div class="col-sm-8">
-                            <input class="form-control" name="telephone"
-                                   value="{{ Auth::user()->telephone }}">
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label class="col-sm-4 col-form-label">{{ __('Evening Telephone') }}</label>
-                        <div class="col-sm-8">
-                            <input class="form-control" name="evening_telephone"
-                                   value="{{ Auth::user()->evening_telephone }}">
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label class="col-sm-4 col-form-label">{{ __('Fax') }}</label>
-                        <div class="col-sm-8">
-                            <input class="form-control" name="fax" value="{{ Auth::user()->fax }}">
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label class="col-sm-4 col-form-label">{{ __('Mobile') }}</label>
-                        <div class="col-sm-8">
-                            <input class="form-control" name="mobile" value="{{ Auth::user()->mobile }}">
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label class="col-sm-4 col-form-label">{{ __('Email') }}</label>
-                        <div class="col-sm-8">
-                            <input class="form-control-plaintext" placeholder="{{ Auth::user()->email }}">
-                            <small class="form-text text-danger">{{ __('To update your account email, please contact the sales office.') }}</small>
+                        <div class="ml-2 w-full">
+                            <label for="mobile" class="text-sm">{{ __('mobile') }}</label>
+                            <input id="mobile" name="telephone" class="bg-gray-100"
+                                   value="{{ old('mobile') ?: auth()->user()->mobile }}">
                         </div>
                     </div>
 
                     <div class="text-right">
-                        <button class="btn btn-primary">{{ __('Save Changes') }}</button>
+                        <button class="button button-primary">{{ __('Update') }}</button>
                     </div>
                 </form>
             </div>
+        </div>
+    </div>
+
+    <div class="bg-white rounded shadow-md p-3 mb-5 mr-2">
+        <div class="flex">
+            <div class="w-1/2">
+                <h4 class="text-primary">{{ __('Invoice Address') }}</h4>
+
+                <div>{{ auth()->user()->customer->invoice_name }}</div>
+                <div>{{ auth()->user()->customer->invoice_address_line_1 }}</div>
+                <div>{{ auth()->user()->customer->invoice_address_line_2 }}</div>
+                <div>{{ auth()->user()->customer->invoice_address_line_3 }}</div>
+                <div>{{ auth()->user()->customer->invoice_address_line_4 }}</div>
+                <div>{{ auth()->user()->customer->invoice_address_line_5 }}</div>
+            </div>
+            <div class="w-1/2">
+                <h4 class="text-primary">{{ __('Default Delivery Address') }}</h4>
+
+                @if ($default_address)
+                    <div>{{ $default_address->address_line_1 }}</div>
+                    <div>{{ $default_address->address_line_2 }}</div>
+                    <div>{{ $default_address->address_line_3 }}</div>
+                    <div>{{ $default_address->address_line_4 }}</div>
+                    <div>{{ $default_address->address_line_5 }}</div>
+                    <div>{{ $default_address->post_code }}</div>
+                @else
+                    <span class="text-muted">
+                        {{ __('No default delivery address set, click below to add one.') }}
+                    </span>
+                @endif
+
+                <div class="text-right mt-2">
+                    <a href="{{ route('account.addresses') }}">
+                        <button class="button button-inverse">{{ __('View Addresses') }}</button>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="bg-white rounded shadow-md p-3 mb-5 mr-2">
+        <h4 class="text-primary">{{ __('API Access') }}</h4>
+
+        <p>{{ __('Below is the unique API key to give you access to the online ordering portal API') }}</p>
+
+        <div class="flex justify-between items-center bg-gray-100 text-gray-600 mt-3 p-4 rounded">
+            <span>{{ auth()->user()->api_token }}</span>
+            <span class="badge badge-info cursor-pointer">{{ __('Click to copy') }}</span>
         </div>
     </div>
 @endsection
