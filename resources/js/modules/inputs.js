9b33d4b0747957c9cfb5012e890f851b406493ca
Samuel Loft <sam@devloft.co.uk>
2019-12-06 16:21:58 +0000

    Removed JS file that has been replaced with a vue component

--- resources/js/modules/inputs.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $('input[name="date_from"]').datepicker({
-        dateFormat: 'dd/mm/yy'
-    });
-
-    $('input[name="date_to"]').datepicker({
-        dateFormat: 'dd/mm/yy'
-    });
-});
