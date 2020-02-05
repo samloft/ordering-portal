d60ebe6b263cba7f6889deed2129eecc0da56486
Samuel Loft <sam@devloft.co.uk>
2020-02-05 15:12:31 +0000

    Added token to password reset

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
     <form method="POST" action="{{ route('password.update') }}">
-        @csrf
+        <input type="hidden" name="token" value="{{ $token }}">
 
         <div class="input-group">
             <label for="email">{{ __('Email address') }}</label>
