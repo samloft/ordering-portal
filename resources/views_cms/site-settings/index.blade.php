f45adb1ee4fd1fc397a1e1ef2b0160cdc07de130
Samuel Loft <sam@devloft.co.uk>
2020-02-14 10:50:31 +0000

    Added maintenance global

--- resources/views_cms/site-settings/index.blade.php
+++ resources/views_cms/site-settings/index.blade.php
@@ -5,6 +5,12 @@
 
 @section('content')
     <div class="px-6 pt-4 pb-6 xl:px-10 xl:pt-6 xl:pb-8 bg-white rounded-lg shadow">
+        <div class="text-right">
+            <button class="button bg-gray-800 text-white w-56">Enable site maintenance</button>
+        </div>
+
+        <hr class="mt-3 mb-3">
+
         <div class="flex">
             <div class="w-1/4 mr-6">
                 <h5 class="font-medium text-lg mb-2">Stuff</h5>
