ecc5ad88e505a91614815beacf89a7040dfd382b
Samuel Loft <sam@devloft.co.uk>
2020-02-21 08:15:36 +0000

    Added extra tests for the basket

--- tests/Feature/BasketTest.php
+++ tests/Feature/BasketTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\Setup\BasketFactory;
 use Tests\Setup\UserFactory;
+use App\Models\Price;
+use App\Models\Basket;
 use Tests\TestCase;
 
 class BasketTest extends TestCase
@@ -32,5 +34,74 @@ class BasketTest extends TestCase
                 'reference' => 'test-basket',
             ]);
         }
+    }
+
+    /**
+     * @test
+     */
+    public function a_product_line_can_be_removed(): void
+    {
+        $user = (new UserFactory())->withCustomer()->create();
+
+        $this->signIn($user);
+
+        $product = (new BasketFactory())->create()->first();
+
+        $this->assertDatabaseHas('basket', [
+            'product' => $product->code,
+        ]);
+
+        $this->post(route('basket.delete-line', ['product' => $product->code]));
+
+        $this->assertDatabaseMissing('basket', [
+            'product' => $product->code,
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function a_product_line_can_have_its_qty_updated(): void
+    {
+        $user = (new UserFactory())->withCustomer()->create();
+
+        $this->signIn($user);
+
+        $product = (new BasketFactory())->create()->first();
+
+        $this->post(route('basket.add-product', [
+            'product' => $product->code,
+            'quantity' => 1000,
+            'update' => true,
+        ]));
+
+        $this->assertDatabaseHas('basket', [
+            'product' => $product->code,
+            'quantity' => 1000,
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function bulk_savings_are_shown_if_within_range(): void
+    {
+        $user = (new UserFactory())->withCustomer()->create();
+
+        $this->signIn($user);
+
+        $product = (new BasketFactory())->create()->first();
+
+        Price::where('customer_code', $user->customer->code)->where('product', $product->code)->update([
+            'price' => 10.00,
+            'price1' => 5.00,
+            'break1' => 500,
+        ]);
+
+        Basket::where('user_id', $user->id)->where('product', $product->code)->update([
+            'quantity' => 450,
+        ]);
+
+        $this->get(route('basket'))->assertSee('potential_saving&quot;:true');
     }
 }
