57a67d1f38a1314ef79ac5f88816002840cf979d
Samuel Loft <sam@devloft.co.uk>
2020-02-25 10:41:23 +0000

    Added URL to accept terms checkbox

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -130,7 +130,7 @@
                         <label class="checkbox flex items-center">
                             <input type="checkbox" name="terms" class="form-checkbox"
                                    value="{{ old('terms') ? 'checked' : '' }}" autocomplete="off">
-                            <span class="ml-2">{{ __('I have read and agree to the terms & conditions') }}</span>
+                            <span class="ml-2">I have read and agree to the <a href="{{ route('support.terms') }}" class="underline" target="_blank">terms & conditions</a></span>
                         </label>
                     </div>
 
