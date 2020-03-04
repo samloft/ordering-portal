b9108b76e9289c375024ec2ef6ca305758c7885e
Samuel Loft <sam@devloft.co.uk>
2020-03-04 15:09:22 +0000

    Added the ability to update support pages

--- resources/views/support/index.blade.php
+++ resources/views/support/index.blade.php
@@ -3,9 +3,11 @@
 @section('page.title', ucwords(str_replace('-', ' ', $support->name)))
 
 @section('content')
-    <div class="w-full mb-5 text-center">
-        <h2 class="font-semibold tracking-widest">{{ ucwords(str_replace('-', ' ', $support->name)) }}</h2>
-    </div>
+    <div class="bg-white rounded-lg shadow p-6 markdown">
+        <div class="w-full mb-5 text-center">
+            <h2 class="font-semibold tracking-widest">{{ ucwords(str_replace('-', ' ', $support->name)) }}</h2>
+        </div>
 
-    {!! $support->description !!}
+        {!! $support->description !!}
+    </div>
 @endsection
