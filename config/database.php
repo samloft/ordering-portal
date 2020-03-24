418657179fa8145f460d42d4f9af667551b227e7
Samuel Loft <sam@devloft.co.uk>
2020-03-24 15:55:47 +0000

    Removed commented code

--- config/database.php
+++ config/database.php
@@ -56,22 +56,6 @@ return [
             'engine'         => null,
         ],
 
-        //'tenant' => [
-        //    'driver'         => 'mysql',
-        //    'host'           => env('DB_HOST', '127.0.0.1'),
-        //    'port'           => env('DB_PORT', '3306'),
-        //    'database'       => env('DB_DATABASE', 'laravel'),
-        //    'username'       => env('DB_USERNAME', 'forge'),
-        //    'password'       => env('DB_PASSWORD', ''),
-        //    'unix_socket'    => env('DB_SOCKET', ''),
-        //    'charset'        => 'utf8mb4',
-        //    'collation'      => 'utf8mb4_unicode_ci',
-        //    'prefix'         => '',
-        //    'prefix_indexes' => true,
-        //    'strict'         => true,
-        //    'engine'         => null,
-        //],
-
         'testing' => [
             'driver'         => 'mysql',
             'host'           => env('DB_HOST', '127.0.0.1'),
