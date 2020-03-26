35abcb54491fb70545fbed47e986d789c3789cba
Samuel Loft <sam@devloft.co.uk>
2020-03-26 10:51:45 +0000

    Removed public from URL

--- config/filesystems.php
+++ config/filesystems.php
@@ -52,7 +52,7 @@ return [
             'driver' => 'local',
             'root' => storage_path('app/public'),
             //'root'       => public_path(),
-            'url' => env('APP_URL').'/public',
+            'url' => env('APP_URL').'/',
             'visibility' => 'public',
         ],
 
