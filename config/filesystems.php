d75cb41ea81eff27332ce17ae796fe0abcacb05b
Samuel Loft <sam@devloft.co.uk>
2020-03-26 11:07:16 +0000

    Changes public storage back to public path (again)

--- config/filesystems.php
+++ config/filesystems.php
@@ -50,8 +50,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            //'root'       => public_path(),
+            'root' => public_path(),
             'url' => env('APP_URL').'/',
             'visibility' => 'public',
         ],
