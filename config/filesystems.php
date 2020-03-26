ae30b7d2717a93e13e7853710735faa8af9a6981
Samuel Loft <sam@devloft.co.uk>
2020-03-26 10:44:49 +0000

    Reverted root back

--- config/filesystems.php
+++ config/filesystems.php
@@ -50,8 +50,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            //'root' => storage_path('app/public'),
-            'root'       => public_path(),
+            'root' => storage_path('app/public'),
+            //'root'       => public_path(),
             'url' => env('APP_URL').'/public',
             'visibility' => 'public',
         ],
