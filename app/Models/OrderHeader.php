3d88f2777e63301de633c6dba63c3d696109068c
Samuel Loft <sam@devloft.co.uk>
2020-03-27 16:12:37 +0000

    Cleaned up the order header model

--- app/Models/OrderHeader.php
+++ app/Models/OrderHeader.php
@@ -8,7 +8,29 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 /**
  * App\Models\OrderHeader.
  *
- * @mixin Eloquent
+ * @mixin \Eloquent
+ *
+ * @property string $order_number
+ * @property string $customer_code
+ * @property int $user_id
+ * @property string $reference
+ * @property string $notes
+ * @property string $name
+ * @property string $telephone
+ * @property string $mobile
+ * @property string $address_line_1
+ * @property string $address_line_2
+ * @property string $address_line_3
+ * @property string $address_line_4
+ * @property string $address_line_5
+ * @property string $delivery_method
+ * @property string $delivery_code
+ * @property float $delivery_cost
+ * @property float $small_order_charge
+ * @property float $value
+ * @property int $imported
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $deleted_at
  */
 class OrderHeader extends Model
 {
@@ -22,6 +44,8 @@ class OrderHeader extends Model
     protected $fillable = [];
 
     /**
+     * Return all the line for the order.
+     *
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
     public function lines(): HasMany
