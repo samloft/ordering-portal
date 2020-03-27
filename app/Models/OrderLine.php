89fd8b185efbeef686ffcf7b8ce23655bcac5cff
Samuel Loft <sam@devloft.co.uk>
2020-03-27 15:54:30 +0000

    Cleaned up order line model

--- app/Models/OrderLine.php
+++ app/Models/OrderLine.php
@@ -7,7 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * App\Models\OrderLine.
  *
- * @mixin Eloquent
+ * @mixin \Eloquent
+ *
+ * @property string $order_number
+ * @property string $product
+ * @property string $description
+ * @property int $quantity
+ * @property string $stock_type
+ * @property float $price
+ * @property float $total
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
  */
 class OrderLine extends Model
 {
