300d41b71ea67f43d19f12eb96f3142dab3b7bd3
Samuel Loft <sam@devloft.co.uk>
2020-03-31 08:33:29 +0100

    Added ignore for browser middleware if testing

--- app/Http/Middleware/BrowserMiddleware.php
+++ app/Http/Middleware/BrowserMiddleware.php
@@ -15,6 +15,10 @@ class BrowserMiddleware
      */
     public function handle($request, Closure $next)
     {
+        if (env('APP_ENV') === 'testing') {
+            return $next($request);
+        }
+        
         if (request()->route()->getName() !== 'not-supported') {
             if (preg_match('~MSIE|Internet Explorer~i', $_SERVER['HTTP_USER_AGENT']) && (strpos($_SERVER['HTTP_USER_AGENT'], 'Trident/7.0; rv:11.0') !== true)) {
                 return redirect(route('not-supported'));
