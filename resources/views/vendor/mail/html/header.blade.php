3eb54d9528e97578a3532ef16508ed1b285b0464
Samuel Loft <sam@devloft.co.uk>
2020-04-02 13:53:08 +0100

    Improved styling for html emails

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,11 +1,7 @@
 <tr>
-<td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
-@else
-{{ $slot }}
-@endif
-</a>
-</td>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            <img src="{{ asset('images/logos/logo-'.config('app.name').'-dark.png') }}" class="logo" alt="{{ ucfirst(config('app.name')) }} Ordering Portal">
+        </a>
+    </td>
 </tr>
