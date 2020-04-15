dbb4cd9ca0fd619d077bb68d9b493d7eb9c6bc85
Samuel Loft <sam@devloft.co.uk>
2020-04-15 11:05:16 +0100

    Added order upload config into the CMS

---
+++ database/migrations/2020_04_15_093946_add_upload_settings_to_globals_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUploadSettingsToGlobalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        DB::table('globals')->insert([
+            'key' => 'upload-config',
+            'value' => json_encode([
+                'prices' => true,
+                'packs' => false,
+            ], true),
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        DB::table('globals')->where('key', 'upload-config')->delete();
+    }
+}
