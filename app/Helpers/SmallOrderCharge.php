675e6795afde93ab915cb9bd46f2b01f07628faa
Samuel Loft <sam@devloft.co.uk>
2020-04-27 12:27:09 +0100

    Cleaned up small order charge check

--- app/Helpers/SmallOrderCharge.php
+++ app/Helpers/SmallOrderCharge.php
@@ -16,21 +16,12 @@ function smallOrderCharge($order_value, $delivery = null)
 
     $charge = $small_order_data['charge'];
 
-    if ($order_value > $small_order_data['threshold']) {
+    if (!$delivery && $order_value > $small_order_data['threshold']) {
         $charge = 0.00;
-    }
-
-    // If a delivery is passed, we must be on the checkout/checking out.
-    if ($delivery) {
-        // If we exclude small order charges on collections return 0.
-        if ($small_order_data['exclude_on_collection'] && str_contains(strtoupper($delivery->title), 'COLLECT')) {
-            $charge = 0.00;
-        }
-
-        // If we exclude small order when a delivery is payed for return 0.
-        if ($small_order_data['exclude_on_charge_delivery'] && $delivery->price > 0) {
-            $charge = 0.00;
-        }
+    } elseif ($delivery && $small_order_data['exclude_on_collection'] && str_contains(strtoupper($delivery->title), 'COLLECT')) {
+        $charge = 0.00;
+    } elseif ($delivery && $small_order_data['exclude_on_charge_delivery'] && $delivery->price > 0) {
+        $charge = 0.00;
     }
 
     return [
