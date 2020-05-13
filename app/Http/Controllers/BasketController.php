34053bdef0a02c04b9c8c23dee20fe23da885594
Samuel Loft <sam@devloft.co.uk>
2020-05-13 10:38:22 +0100

    Removed storage as no longer required

--- app/Http/Controllers/BasketController.php
+++ app/Http/Controllers/BasketController.php
@@ -11,7 +11,6 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Response;
 use Illuminate\View\View;
-use Storage;
 
 class BasketController extends Controller
 {
