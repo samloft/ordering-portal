275f4a7d4dd5f11bf11a69f7b17fc9310b0ab8a7
Samuel Loft <sam@devloft.co.uk>
2020-05-19 10:17:14 +0100

    Added loading animation to basket value in header

--- resources/js/modules/BasketDropdown.vue
+++ resources/js/modules/BasketDropdown.vue
@@ -14,7 +14,27 @@
         </div>
         <div class="hidden lg:block ml-2 text-left flex mr-5">
             <span class="text-sm font-thin">My Cart</span>
-            <div class="font-light">{{ basketValue }}</div>
+            <div class="font-light">
+                <span v-if="! basketUpdating">{{ basketValue }}</span>
+
+                <svg v-if="basketUpdating" xmlns="http://www.w3.org/2000/svg"
+                     width="20px" height="20px" viewBox="0 0 40 40" enable-background="new 0 0 40 40"
+                     xml:space="preserve" class="ml-3">
+                    <path opacity="0.2" fill="#fff" d="M20.201,5.169c-8.254,0-14.946,6.692-14.946,14.946c0,8.255,6.692,14.946,14.946,14.946
+                                  s14.946-6.691,14.946-14.946C35.146,11.861,28.455,5.169,20.201,5.169z M20.201,31.749c-6.425,0-11.634-5.208-11.634-11.634
+                                  c0-6.425,5.209-11.634,11.634-11.634c6.425,0,11.633,5.209,11.633,11.634C31.834,26.541,26.626,31.749,20.201,31.749z"/>
+                    <path fill="#000" d="M26.013,10.047l1.654-2.866c-2.198-1.272-4.743-2.012-7.466-2.012h0v3.312h0
+                                  C22.32,8.481,24.301,9.057,26.013,10.047z">
+                        <animateTransform attributeType="xml"
+                                          attributeName="transform"
+                                          type="rotate"
+                                          from="0 20 20"
+                                          to="360 20 20"
+                                          dur="0.5s"
+                                          repeatCount="indefinite"/>
+                    </path>
+                </svg>
+            </div>
         </div>
 
         <div @mouseenter="mouseOver" @mouseleave="mouseOut" class="hidden md:block">
@@ -56,7 +76,8 @@
             return {
                 dropdown: false,
                 products: {},
-                basketValue: 'Checking...',
+                basketValue: '',
+                basketUpdating: true,
                 basketItems: 0,
             }
         },
@@ -89,6 +110,8 @@
                 axios.get('/basket/summary')
                     .then(function (response) {
                         if (response) {
+                            self.basketUpdating = false;
+
                             self.basketValue = response.data.summary.goods_total;
                             self.basketItems = response.data.line_count;
                         }
@@ -106,10 +129,14 @@
             var self = this;
 
             Event.$on('product-added', function (data) {
+                self.basketUpdating = true;
+
                 self.basketSummary();
             });
 
             Event.$on('product-updated', function (data) {
+                self.basketUpdating = true;
+
                 self.basketSummary();
             });
 
