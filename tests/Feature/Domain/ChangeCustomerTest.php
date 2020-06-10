f8e557fcadaf1a48b1b30b8e3b1c834e24366a42
Samuel Loft <sam@devloft.co.uk>
2020-06-10 08:10:00 +0100

    Autocomplete test for customers

--- tests/Feature/Domain/ChangeCustomerTest.php
+++ tests/Feature/Domain/ChangeCustomerTest.php
@@ -79,3 +79,14 @@ test('admins cannot switch to a customer that no longer exist', function () {
 
     $this->post('/account/customer/change', ['customer', 'ABC123'])->assertStatus(302)->assertSessionHas('error');
 });
+
+test('is auto completed', function () {
+    $user = (new UserFactory())->withCustomer()->withUserCustomers(2)->create([
+        'admin' => 1,
+    ]);
+
+    actingAs($user);
+
+    $this->post(route('customer.autocomplete', ['customer' => substr($user->customers->first()->code, 0, 3)]))
+        ->assertOk()->assertSee($user->customers->first()->code);
+});
