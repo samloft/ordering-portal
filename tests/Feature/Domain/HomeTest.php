f48c61d2e596221c807370761a376034b43379e6
Samuel Loft <sam@devloft.co.uk>
2020-06-11 13:55:37 +0100

    Test for browser middleware

--- tests/Feature/Domain/HomeTest.php
+++ tests/Feature/Domain/HomeTest.php
@@ -9,3 +9,23 @@ test('returns an ok response', function () {
 
     $this->get('/')->assertStatus(200);
 });
+
+test('IE11 is the only IE version allowed', function () {
+    //$mock = $this->getMockBuilder(\App\Http\Middleware\BrowserMiddleware::class)
+    //    ->setMethods(['getUserAgent'])
+    //    ->getMock();
+    //
+    //$mock->expects($this->once())->method('getUserAgent')->willReturn('MSIE|Internet Explorer Trident/7.0; rv:10.0');
+    //
+    //$this->assertSame('', $mock->handle()->andReturnsUsing(function ($request, \Closure $next) {
+    //    return $next($request);
+    //}));
+
+    $user = (new UserFactory())->withCustomer()->create();
+
+    actingAs($user);
+
+    $this->get('/', [
+        'HTTP_USER_AGENT' => 'MSIE|Internet Explorer Trident/7.0; rv:10.0',
+    ])->assertRedirect(route('not-supported'));
+});
